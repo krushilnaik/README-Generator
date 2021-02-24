26b2f58329a290a749422b0b1968ca56ede1acc0
Krushil <krushilnaik96@gmail.com>
2021-02-23 20:10:43 -0500

    get the prompts up

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
 // TODO: Include packages needed for this application
+// import writeFile from 'fs';
+// import prompt from 'inquirer';
+// import generateMarkdown from './utils/generateMarkdown';
+
+var fs = require('fs');
+var inquirer = require('inquirer');
+var generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	{
+		name: "title", type: "input",
+		message: "What's the title of your project?"
+	},
+	{
+		name: "description", type: "input",
+		message: "Give a short description of your project:"
+	},
+	{
+		name: "license", type: "input",
+		message: "What kind of license should your project have?"
+	},
+	{
+		name: "dependencies", type: "input",
+		message: "Command to install dependencies:"
+	},
+	{
+		name: "tests", type: "input",
+		message: "Command to run tests:"
+	},
+	{
+		name: "usage", type: "input",
+		message: "How is this repo used?"
+	},
+	{
+		name: "contributions", type: "input",
+		message: "How can someone contribute?"
+	},
+	{
+		name: "username", type: "input",
+		message: "What's your GitHub username?"
+	},
+	{
+		name: "email", type: "input",
+		message: "What's your email?"
+	},
+];
 
-// TODO: Create a function to write README file
+/**
+ * Write given markdown to 'filename'.md
+ * @param {string} fileName file (without the .md) to write the markdown to
+ * @param {string} data raw markdown content to be written to file
+ */
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+/**
+ * Initialize app
+ */
+function init() {
+	inquirer.prompt(questions).then(_ => console.log("Generating README..."));
+}
 
 // Function call to initialize app
 init();
